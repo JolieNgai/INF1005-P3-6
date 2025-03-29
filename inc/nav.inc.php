e93f0b8d91f708067d052c124651075fee75f8ec
Si Pei <tohsipei@yahoo.com.sg>
2025-03-29 15:06:43 +0800

    Removed unnecessary code

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -14,7 +14,7 @@
 <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
      <li class="nav-item">
-        <a class="nav-link" href="../index.php">Homepage</a>
+        <a class="nav-link" href="/index.php">Homepage</a>
      </li>
      <li class = "nav-item">
         <a class ="nav-link" href="FoodCalc/calculator.php">Calculator</a> <!-- Add in link for this page -->
